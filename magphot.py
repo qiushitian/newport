f0f0a96cedde9237df7b3a2390ba3b819f423ef6
Chris Tian <98070235+qiushitian@users.noreply.github.com>
2025-02-10 23:33:45 -0500

    Update magphot, draft version Kahle+
    This version produced the first iteration of plots in Kahle et al. Overleaf and email to Seth on 20250207

--- magphot.py
+++ magphot.py
@@ -12,46 +12,80 @@ from astroquery.utils.tap.core import TapPlus
 SIMBAD_TAP = TapPlus(url="http://simbad.u-strasbg.fr/simbad/sim-tap")
 
 # Percentile of non-NaN observations required for column to be considered valid
-# CRITERION = 70
+CRITERION = 0.70
 N_COL_HEAD = 5  # TODO replace with str.isdigit()
+COMBINED_KEY = 'combined'
+
+
+def combine_mags(mags):
+    mags = np.array([_ for _ in mag_dict.values()])
+    if np.isnan(mags).any():
+        raise ValueError('NaN is in the input.')
+
+    fluxes = 10 ** (mags / -2.5)
+    total_flux = np.sum(fluxes)
+    if total_flux <= 0 or np.isnan(total_flux):
+        raise RuntimeError('Total flux is 0 or NaN.')
+
+    return -2.5 * np.log10(total_flux)
+
 
 for fn in TARGET_FN:
     if '86226' not in fn:
         continue
 
+    phot_table_all_band = table.Table.read(f'tables/phot/photometry_{fn}.fits')
+
     for band in ['B', 'V', 'R', 'I']:
-        # (f'tables/onerel/binned_onerel_{fn}_{band}.fits')
-        onerel_table = table.Table.read(f'tables/onerel/onerel_{fn}_{band}.fits')
+        phot_table = phot_table_all_band[phot_table_all_band['band'] == band]
+
+        if len(phot_table) < 1:
+            print(f'{fn}\t{band}\t skipped')
+            continue
 
         valid_colnames = []
-        for colname in onerel_table.colnames[N_COL_HEAD:]:
-            if colname != TARGET_GAIA_DR3[fn]:
+        for colname in phot_table.colnames[N_COL_HEAD:]:
+            if (
+                    isinstance(phot_table[colname], table.Table.MaskedColumn)
+                    and phot_table[colname].mask.sum() / len(phot_table) < (1 - CRITERION)
+                    and colname != TARGET_GAIA_DR3[fn]
+            ):
                 valid_colnames.append(colname)
+        phot_table = phot_table.filled(np.nan)
 
-        print(f'{fn}\t{band}\t{len(valid_colnames)}')  # DEV
+        print(f'{fn}\t{band}\tvalid\t{len(valid_colnames)}')  # TODO DEV
 
         # valid_table = phot_table[valid_colnames]
 
-        mag_table = table.Table()  # names=valid_table.colnames
+        mag_dict = {}
         for colname in valid_colnames:
             mag = SIMBAD_TAP.launch_job(
                 f"SELECT {band} FROM allfluxes JOIN ident USING(oidref) WHERE id = 'Gaia DR3 {colname}';"
             ).get_results()[band]
-            if len(mag) > 0 and mag is not np.ma.masked:
-                mag_table.add_column((- 2.5 * np.log10(onerel_table[colname]) + mag[0]) * u.mag, name=colname)
+            if len(mag) > 0 and not mag.mask:
+                mag_dict[colname] = mag[0]
 
-        mag_table = table.hstack([onerel_table[onerel_table.colnames[: N_COL_HEAD]], mag_table])
+        print(f'{fn}\t{band}\twith mag\t{len(mag_dict)}')  # TODO DEV
 
-        # gpb = onerel_table.group_by(['night', 'band'])
-        # binned_onerel = gpb.groups.aggregate(np.median)
-        # # TODO add std or mad_std here and save it
+        combined_counts = np.array([0.0] * len(phot_table))
+        onerel_table = table.Table()  # names=valid_table.colnames
+        for colname in mag_dict.keys():
+            combined_counts += phot_table[colname]
+            onerel_table[colname] = phot_table[TARGET_GAIA_DR3[fn]] / phot_table[colname]
+        onerel_table[COMBINED_KEY] = phot_table[TARGET_GAIA_DR3[fn]] / combined_counts
+        mag_dict[COMBINED_KEY] = combine_mags(mag_dict.values())
 
-        # band-night normalization
-        # for colname in binned_onerel.colnames:
-        #     if colname.isdigit():
-        #         med = np.nanmedian(binned_onerel[colname])
-        #         binned_onerel[colname] /= med
-        #         onerel_table[colname] /= med
-        #
-        # binned_onerel.write(f'tables/onerel/binned_onerel_{fn}_{band}.fits', overwrite=False)
-        mag_table.write(f'tables/mag/mag_{fn}_{band}.fits', overwrite=True)
+        mag_table = table.Table()  # names=valid_table.colnames
+        for colname, mag in mag_dict.items():
+            target_mag = -2.5 * np.log10(onerel_table[colname]) + mag
+            mag_table[colname] = target_mag * u.mag
+
+        # add mean column
+        mean_mag_array = []
+        for col in mag_table.colnames:
+            if col != COMBINED_KEY:
+                mean_mag_array.append(mag_table[col])
+        mag_table['mean'] = np.mean(mean_mag_array, axis=0)
+
+        mag_table = table.hstack([phot_table[phot_table.colnames[: N_COL_HEAD]], mag_table])
+        mag_table.write(f'tables/mag/mag_{fn}_{band}.fits', overwrite=False)
--- newport.py
+++ newport.py
@@ -3,10 +3,10 @@ Newport functional methods
 """
 
 import numpy as np
-import astropy.time as time
+from datetime import datetime
+from astropy.time import Time
 import urllib.request
 import xml.etree as etree
-from datetime import datetime
 
 TARGET_FN = [
     'HD_191939',
@@ -19,113 +19,173 @@ TARGET_FN = [
     'TOI-1410',
 ]
 
-TARGET_GAIA_DR3 = {'HD_86226' : '5660492297395345408',
-                   'HD_191939' : '2248126315275354496',
-                   'TOI-1759' : '2216420110788943744',
-                   'TOI-178' : '2318295979126499200',
-                   'TOI-431' : '2908664557091200768',
-                   'TOI-561' : '3850421005290172416',
-                   'TOI-1410' : '1958584565350234752',
-                   'TOI-1201' : '5157183324996790272'}
+TARGET_GAIA_DR3 = {'HD_86226': '5660492297395345408',
+                   'HD_191939': '2248126315275354496',
+                   'TOI-1759': '2216420110788943744',
+                   'TOI-178': '2318295979126499200',
+                   'TOI-431': '2908664557091200768',
+                   'TOI-561': '3850421005290172416',
+                   'TOI-1410': '1958584565350234752',
+                   'TOI-1201': '5157183324996790272'}
 
 COLORS = {'B': 'C0', 'V': 'C2', 'R': 'C3', 'I': 'maroon'}
 MARKERS = {'B': 'o', 'V': 'x', 'R': 's', 'I': 'D'}
 
-def hst_visits():
-    """
-    Process StSci HST GO 17192 XML status
 
-    :return: time at which XML is fetched, dict of past visits, dict of future visits
+def get_hst(target: str, path=None, url=None, future=False):
     """
-    archived = {}
-    future = {}
+    Returns:
+        (wfc3 mid time, stis mid time)
+    """
+    if path:
+        with open(path, 'r') as f:
+            xml_string = f.read()
+    elif url:
+        if url == '17192' or url == 17192:
+            url = 'https://www.stsci.edu/cgi-bin/get-visit-status?id=17192&markupFormat=xml&observatory=HST'
+        elif url == '17414' or url == 17414:
+            url = 'https://www.stsci.edu/cgi-bin/get-visit-status?id=17414&markupFormat=xml&observatory=HST'
+        with urllib.request.urlopen(url) as response:
+            xml_string = response.read().decode('utf-8')
+    else:
+        raise ValueError('`path` and `url` cannot be both None.')
 
-    url = 'https://www.stsci.edu/cgi-bin/get-visit-status?id=17192&markupFormat=xml&observatory=HST'
-    with urllib.request.urlopen(url) as response:
-        xml_string = response.read().decode('utf-8')
     root = etree.ElementTree.fromstring(xml_string)
 
+    target = target.replace('_', '-').replace(' ', '-')
+
     # iter around visits
+    wfc3_mid_time = []
+    stis_mid_time = []
     for v in root.iterfind('visit'):
+        # visit status screening
         status = v.find('status').text
-        target = v.find('target').text
-
-        # keep only WFC3 observation
-        config = v.find('configuration').text
-        if not config.startswith('WFC3'):
+        if not future and status != 'Executed':
             continue
-        print(target, config)
 
-        # find archived (done) visits
-        starts = []
-        for t in v.iterfind('startTime'):
-            starts.append(datetime.strptime(t.text, '%b %d, %Y %H:%M:%S'))
-        ends = []
-        for t in v.iterfind('endTime'):
-            ends.append(datetime.strptime(t.text, '%b %d, %Y %H:%M:%S'))
-        for s, e in zip(starts, ends):
-            if target not in archived:
-                archived[target] = []
-            archived[target].append((s, e))
-
-        # find future plan windows
-        for t in v.iterfind('planWindow'):
-            start_end = t.text[:-22].split(' - ')
-            start = datetime.strptime(start_end[0], '%b %d, %Y')
-            end = datetime.strptime(start_end[1], '%b %d, %Y')
-            if target not in future:
-                future[target] = []
-            future[target].append((start, end))
-
-    url2 = 'https://www.stsci.edu/cgi-bin/get-visit-status?id=17414&markupFormat=xml&observatory=HST'
-    with urllib.request.urlopen(url2) as response:
-        xml_string2 = response.read().decode('utf-8')
-    root2 = etree.ElementTree.fromstring(xml_string2)
-
-    # iter around visits
-    for v in root2.iterfind('visit'):
-        status = v.find('status').text
-        target = v.find('target').text
-
-        # keep only WFC3 observation
-        config = v.find('configuration').text
-        if not config.startswith('WFC3'):
+        # visit target screening
+        visit_target = v.find('target').text
+        if visit_target != target:
             continue
-        print(target, config)
 
-        # find archived (done) visits
-        starts = []
-        for t in v.iterfind('startTime'):
-            starts.append(datetime.strptime(t.text, '%b %d, %Y %H:%M:%S'))
-        ends = []
-        for t in v.iterfind('endTime'):
-            ends.append(datetime.strptime(t.text, '%b %d, %Y %H:%M:%S'))
-        for s, e in zip(starts, ends):
-            if target not in archived:
-                archived[target] = []
-            archived[target].append((s, e))
+        # find mid time
+        start_time = datetime.strptime(v.find('startTime').text, '%b %d, %Y %H:%M:%S')
+        end_time = datetime.strptime(v.find('endTime').text, '%b %d, %Y %H:%M:%S')
+        midpoint = Time(start_time + (end_time - start_time) / 2)
 
-        # find future plan windows
-        for t in v.iterfind('planWindow'):
-            start_end = t.text[:-22].split(' - ')
-            start = datetime.strptime(start_end[0], '%b %d, %Y')
-            end = datetime.strptime(start_end[1], '%b %d, %Y')
-            if target not in future:
-                future[target] = []
-            future[target].append((start, end))
+        # goes into wfc3 or stis mid time list
+        config = v.find('configuration').text
+        if config.startswith('WFC3'):
+            wfc3_mid_time.append(midpoint)
+        elif config.startswith('STIS'):
+            stis_mid_time.append(midpoint)
+        else:
+            raise ValueError(f'Unknown configuration: {config}')
 
-    # get time at which the information is fetched
-    report_time = datetime.strptime(root2.find('reportTime').text, '%a %b %d %H:%M:%S %Z %Y')
+    print(f'HST visit report time: {root.find("reportTime").text}')
 
-    # print(report_time.strftime("%m/%d/%Y %H:%M"))
+    return wfc3_mid_time, stis_mid_time
 
-    return report_time, archived, future
 
-def plot_hst(id, dict, ax, c):
-    times = np.array(dict.get(id.replace(' ', '-')))
-    if len(times.shape) > 1:
-        for s, e in times:
-            mid = s + (e - s) / 2
-            ax.axvline(time.Time(mid).mjd, c=c, linewidth=0.3, alpha=1)
-            # ax.axvline(Time(s).mjd, c=c, linewidth=0.1, alpha=0.8)
-            # ax.axvline(Time(e).mjd, c=c, linewidth=0.1, alpha=0.8)
+# --- Below are old `hst_visits` and `plot_hst` that works together.     --- #
+# --- They are both obsolete and `hst_visits` is replaced with `get_hst` --- #
+
+# def hst_visits():
+#     """
+#     Process StSci HST GO 17192 XML status
+#
+#     :return: time at which XML is fetched, dict of past visits, dict of future visits
+#     """
+#     archived = {}
+#     future = {}
+#
+#     url = 'https://www.stsci.edu/cgi-bin/get-visit-status?id=17192&markupFormat=xml&observatory=HST'
+#     with urllib.request.urlopen(url) as response:
+#         xml_string = response.read().decode('utf-8')
+#     root = etree.ElementTree.fromstring(xml_string)
+#
+#     # iter around visits
+#     for v in root.iterfind('visit'):
+#         status = v.find('status').text
+#         target = v.find('target').text
+#
+#         # keep only WFC3 observation
+#         config = v.find('configuration').text
+#         if not config.startswith('WFC3'):
+#             continue
+#         print(target, config)
+#
+#         # find archived (done) visits
+#         starts = []
+#         for t in v.iterfind('startTime'):
+#             starts.append(datetime.strptime(t.text, '%b %d, %Y %H:%M:%S'))
+#         ends = []
+#         for t in v.iterfind('endTime'):
+#             ends.append(datetime.strptime(t.text, '%b %d, %Y %H:%M:%S'))
+#         for s, e in zip(starts, ends):
+#             if target not in archived:
+#                 archived[target] = []
+#             archived[target].append((s, e))
+#
+#         # find future plan windows
+#         for t in v.iterfind('planWindow'):
+#             start_end = t.text[:-22].split(' - ')
+#             start = datetime.strptime(start_end[0], '%b %d, %Y')
+#             end = datetime.strptime(start_end[1], '%b %d, %Y')
+#             if target not in future:
+#                 future[target] = []
+#             future[target].append((start, end))
+#
+#     url2 = 'https://www.stsci.edu/cgi-bin/get-visit-status?id=17414&markupFormat=xml&observatory=HST'
+#     with urllib.request.urlopen(url2) as response:
+#         xml_string2 = response.read().decode('utf-8')
+#     root2 = etree.ElementTree.fromstring(xml_string2)
+#
+#     # iter around visits
+#     for v in root2.iterfind('visit'):
+#         status = v.find('status').text
+#         target = v.find('target').text
+#
+#         # keep only WFC3 observation
+#         config = v.find('configuration').text
+#         if not config.startswith('WFC3'):
+#             continue
+#         print(target, config)
+#
+#         # find archived (done) visits
+#         starts = []
+#         for t in v.iterfind('startTime'):
+#             starts.append(datetime.strptime(t.text, '%b %d, %Y %H:%M:%S'))
+#         ends = []
+#         for t in v.iterfind('endTime'):
+#             ends.append(datetime.strptime(t.text, '%b %d, %Y %H:%M:%S'))
+#         for s, e in zip(starts, ends):
+#             if target not in archived:
+#                 archived[target] = []
+#             archived[target].append((s, e))
+#
+#         # find future plan windows
+#         for t in v.iterfind('planWindow'):
+#             start_end = t.text[:-22].split(' - ')
+#             start = datetime.strptime(start_end[0], '%b %d, %Y')
+#             end = datetime.strptime(start_end[1], '%b %d, %Y')
+#             if target not in future:
+#                 future[target] = []
+#             future[target].append((start, end))
+#
+#     # get time at which the information is fetched
+#     report_time = datetime.strptime(root2.find('reportTime').text, '%a %b %d %H:%M:%S %Z %Y')
+#
+#     # print(report_time.strftime("%m/%d/%Y %H:%M"))
+#
+#     return report_time, archived, future
+#
+#
+# def plot_hst(id, dict, ax, c):
+#     times = np.array(dict.get(id.replace(' ', '-')))
+#     if len(times.shape) > 1:
+#         for s, e in times:
+#             mid = s + (e - s) / 2
+#             ax.axvline(Time(mid).mjd, c=c, linewidth=0.3, alpha=1)
+#             # ax.axvline(Time(s).mjd, c=c, linewidth=0.1, alpha=0.8)
+#             # ax.axvline(Time(e).mjd, c=c, linewidth=0.1, alpha=0.8)
